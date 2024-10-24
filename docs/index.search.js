0d337ad443dd00292e63cbb0073ada90d5a25151
davidvargasxyz <tdavetech@gmail.com>
2024-10-24 04:52:24 -0700

    update site

--- docs/index.search.js
+++ docs/index.search.js
@@ -96,14 +96,6 @@ var relearn_search_index = [
     "uri": "/categories/"
   },
   {
-    "breadcrumb": "",
-    "content": "",
-    "description": "",
-    "tags": [],
-    "title": "Contents",
-    "uri": "/content/"
-  },
-  {
     "breadcrumb": "Recipes",
     "content": "Creamy Jalepeno Sauce 1 cup vegan mayo 1 tsp onion powder 1 tsp garlic powder 1 tsp smoked paprika 1 tsp chipotle powder 2 tbps pickled jalepeno brine 8 slices pickled jelepenos finely diced Can add citric acid or lime juice for more zip 2-4 tablespoons vegan sour cream Blend ingredients and add to sauce bottle.",
     "description": "Creamy Jalepeno Sauce 1 cup vegan mayo 1 tsp onion powder 1 tsp garlic powder 1 tsp smoked paprika 1 tsp chipotle powder 2 tbps pickled jalepeno brine 8 slices pickled jelepenos finely diced Can add citric acid or lime juice for more zip 2-4 tablespoons vegan sour cream Blend ingredients and add to sauce bottle.",
